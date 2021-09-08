801b58cf38eab4c3f93f0bd7d0c9bb3fa1ebe0c9
AuArgen <argenazizbekuulu3@gmail.com>
2021-09-08 10:03:35 +0600

    Let Go working Argen

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
 
     <!-- custom css file link  -->
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
 
 </head>
 <body>
@@ -55,11 +58,11 @@
 
 <!-- speciality section starts  -->
 
-<section class="speciality" id="speciality">
+<section class="speciality container" id="speciality">
 
     <h1 class="heading"> our <span>speciality</span> </h1>
 
-    <div class="box-container">
+    <div class="box-container row">
 
         <div class="box">
             <img class="image" src="images/s-img-1.jpg" alt="">
@@ -122,7 +125,7 @@
 
     <h1 class="heading"> most <span>popular</span> foods </h1>
 
-    <div class="box-container">
+    <div class="box-container row">
 
         <div class="box">
             <span class="price"> $5 - $20 </span>
